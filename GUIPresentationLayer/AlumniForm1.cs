657c65f87c6fc05e9b0dbbe412dcfe3856068e8c
Linda Dahl <linda.dahl.s@outlook.com>
2020-02-10 12:20:05 +0100

    Forms tillagda

--- GUIPresentationLayer/AlumniForm1.cs
+++ GUIPresentationLayer/AlumniForm1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GUIPresentationLayer
 {
-    public partial class Form1 : Form
+    public partial class AlumniForm1 : Form
     {
-        public Form1()
+        public AlumniForm1()
         {
             InitializeComponent();
         }
