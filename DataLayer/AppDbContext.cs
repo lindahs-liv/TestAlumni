7ed54fa8f18d36f1319ad9ce9cd766a175f7d5a3
Linda Dahl <linda.dahl.s@outlook.com>
2020-02-24 20:17:54 +0100

    Databas igen

--- DataLayer/AppDbContext.cs
+++ DataLayer/AppDbContext.cs
@@ -15,37 +15,38 @@ namespace DataLayer
         // If you wish to target a different database and/or database provider, modify the 'AppDbContext' 
         // connection string in the application configuration file.
 
+
         #region Adds Tables in Database
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-        public virtual DbSet<User> Users { get; set; }
-        public virtual DbSet<Employee> Employees { get; set; }
-        public virtual DbSet<Administrator> Administrators { get; set; }
-        public virtual DbSet<Student> Students { get; set; }
-        public virtual DbSet<Section> Sections { get; set; }
-        public virtual DbSet<BusinessInformatics> BusinessInformatics { get; set; }
-        public virtual DbSet<ComputerScience> ComputerSciences { get; set; }
-        public virtual DbSet<SystemsScience> SystemsSciences { get; set; }
-        public virtual DbSet<Activity> Activities { get; set; }
-        public virtual DbSet<GuestLecture> GuestLectures { get; set; }
-        public virtual DbSet<Section> Section { get; set; }
-        public virtual DbSet<Session> Sessions { get; set; }
-        public  AppDbContext() : base("AppDbContext")
+        public DbSet<User> Users { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Administrator> Administrators { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<BusinessInformatics> BusinessInformatics { get; set; }
+        public DbSet<ComputerScience> ComputerSciences { get; set; }
+        public DbSet<SystemsScience> SystemsSciences { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<GuestLecture> GuestLectures { get; set; }
+        public DbSet<Section> Section { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public AppDbContext() : base("AppDbContext")
         {
         }
-        #endregion
         // Kanske ska det vara såhär?
         // public virtual DbSet<User> MyEntities { get; set; }
-        // eller såhär
+        // eller kanske såhär
         // public DbSet<User> Users {get; set; }
+
+        //DETTA ÄR AUTOMATISKT INLAGT OCH SKA ANTAGLIGEN ANVÄNDAS PÅ NGT SÄTT
+        //public class MyEntity
+        //{
+        //    public int UserId { get; set; }
+        //    public string Name { get; set; }
+        //}
+        #endregion
+
+
     }
-
-
-    //DETTA ÄR AUTOMATISKT INLAGT OCH SKA ANTAGLIGEN ANVÄNDAS PÅ NGT SÄTT
-
-    //public class MyEntity
-    //{
-    //    public int UserId { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
